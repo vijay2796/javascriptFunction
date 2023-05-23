ce15a0dd6ca4d4b0b315ad7a476662a4c39626bc
vijay2796 <133580986+vijay2796@users.noreply.github.com>
2023-05-23 18:23:26 +0530

    Create fibbonaci.js

---
+++ fibbonaci.js
@@ -0,0 +1,13 @@
+    const fib = (n) => {
+  const fibarray = [0,1];
+  
+  for(i=2; i<n; i++){
+  	fibarray.push(fibarray[i-2] + fibarray[i-1]);
+  }
+  // console.log(fibarray)
+	return fibarray;
+}
+
+
+
+console.log(fib(5));
